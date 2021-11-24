1a1b96fbe43fb0aa65ef7d6018a70c8287751588
IvanGrishyn <IvanGrishyn@gmail.com>
2021-11-24 16:23:19 +0200

    изменено:      cd2 	изменено:      conv.c 	удалено:       b1.txt

--- conv.c
+++ conv.c
@@ -11,14 +11,32 @@
 
 hysto * hysto_head=NULL;//корень гистограммы
 
-int hysto_add(int n){ //функция создания нового узла гистограммы
+
+hysto * create_hysto(int n){ //функция создает корень сети
+hysto * p = (hysto*)malloc(sizeof(hysto));//выделяем память
+if (p!=NULL) {//если выделилась
+p->num=n;//задаем первое значение
+p->count=1;}//оно одно
+return p;//возвращаем результат каким бы он ни был
+}
+
+hysto * get_last_hysto(hysto * p){//функция возвращает указатель на верхушку сети
+			//передаем корень
+while (1){//бесконечный цикл
+if (p->next==NULL) return p; //если следующего нет, значит вершина
+p=p->next;}//загружаем следующий узел
+return NULL;// пустая сеть
+}
+
+hysto * hysto_add(int n){ //функция создания нового узла гистограммы
  hysto * p = (hysto*)malloc(sizeof(hysto));//выделяем память
- if (p!=NULL) { //если память выделилась
+ if (p!=NULL) {  //если память выделилась
+ get_last_hysto(hysto_head)->next=p;
  p->num=n; //для этого чила узел
  p->count=1;//таких числе пока что одно
  p->next=NULL;//следующего может не быть
- return 0; }//возвращаем успех
-return 1;//возвращаем неуд
+ return p; }//возвращаем успех
+return NULL;//возвращаем неуд
 }
 
 int  inc_hysto (hysto * p){//функция инкремента узла
@@ -34,7 +52,7 @@ return 1;//иначе надо добавлять
 
 hysto * is_there(int n){//функция проверяет наличие числа в гистограмме
   puts("is there");             //искомое число 
- struct hysto * tp = &hysto_head;//корень
+ hysto * tp = hysto_head;//корень
   puts("is there struct"); 
 while (tp->next!=NULL){//пока есть следующий узел
 puts("is there while"); 
@@ -47,18 +65,22 @@ return NULL;//ничего не найдено
 hysto * alter_hysto(int * p){//функция создания гистограммы
                             //ссылка на область памяти с исходными данными  
 //struct hysto * tp = NULL; //
+puts(" hysto head");
+hysto_head=create_hysto(p[0]);
 puts("alter hysto");
-for (long i=0;i<MemAllocated;i++){ puts("alter for");//проходим по исходным данным
+for (long i=1;i<MemAllocated;i++){ puts("alter for");//проходим по исходным данным
    if (inc_hysto(is_there(p[i]))) //если уже было плюсуем соответсвующий
-   	if (hysto_add(p[i])) return NULL;	//если нет добавляем новый узел
+   	if (hysto_add(p[i])==NULL) return NULL;	//если нет добавляем новый узел
    			}	//если не получилось выход в пустоту
 return hysto_head;//вызвращаем ссылку на корень
 }
 
 
 void print_hysto(hysto * p){
-
-while(p->next!=NULL) printf("%i %li\n",p->num,p->count);
+puts("print_hysto");
+while(p->next!=NULL) {
+	printf("Output - %i %li\n",p->num,p->count);
+	 p=p->next; }
 
 }
 
